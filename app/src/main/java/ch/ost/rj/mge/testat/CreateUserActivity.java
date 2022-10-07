23fa3cc84878b38f8f31b177d61c1dcdb577865a
Fabian <fabian.freitag@ost.ch>
2022-10-07 15:45:13 +0200

    Initial commit

--- app/src/main/java/ch/ost/rj/mge/testat/CreateUserActivity.java
+++ app/src/main/java/ch/ost/rj/mge/testat/CreateUserActivity.java
@@ -10,5 +10,6 @@ public class CreateUserActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_user);
+
     }
 }
